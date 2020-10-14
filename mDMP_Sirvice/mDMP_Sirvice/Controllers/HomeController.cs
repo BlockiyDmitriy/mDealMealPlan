83dd6c501a9690de6da7368f911434ad5c0e88b7
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2020-10-14 14:18:14 +0300

    Add and delete
    Delete file mDMP_Sirvice (ASP.NET);
    Create file Service (ASP.NET Core);

--- mDMP_Sirvice/mDMP_Sirvice/Controllers/HomeController.cs
+++ mDMP_Sirvice/mDMP_Sirvice/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data_Access_Layer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,16 @@ namespace mDMP_Sirvice.Controllers
 {
     public class HomeController : Controller
     {
+        ConnectDbContext db = new ConnectDbContext();
+        /// <summary>
+        /// Получение данных
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
-            return View();
+            return View(db.Category);
         }
     }
 }
---
+++ mDMP_Sirvice/mDMP_Sirvice/Models/CategoryModel.cs
@@ -0,0 +1,13 @@
+﻿using Business_Logic_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mDMP_Sirvice.Models
+{
+    public class CategoryModel
+    {
+        public Category category = new Category();
+    }
+}
