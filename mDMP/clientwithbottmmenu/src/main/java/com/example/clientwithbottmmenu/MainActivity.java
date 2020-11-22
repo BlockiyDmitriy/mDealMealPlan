db9139cb3b0811bd9793e914c61a99a22c20e8e3
BlockiyDmitriy <51110104+BlockiyDmitriy@users.noreply.github.com>
2020-11-22 21:31:04 +0300

    New UI

---
+++ mDMP/clientwithbottmmenu/src/main/java/com/example/clientwithbottmmenu/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.clientwithbottmmenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.clientwithbottmmenu.ui.calendar.CalendarFragment;
+import com.example.clientwithbottmmenu.ui.diary.DiaryAddItem;
+import com.example.clientwithbottmmenu.ui.diary.DiaryFragment;
+import com.example.clientwithbottmmenu.ui.profile.ProfileFragment;
+import com.example.clientwithbottmmenu.ui.statistic.StatisticActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    FrameLayout container;
+    FragmentManager myFragmentManager;
+
+    DiaryFragment diaryFragment;
+    ProfileFragment profileFragment;
+    CalendarFragment calendarFragment;
+
+    Bundle bundle;
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        container = (FrameLayout) findViewById(R.id.container);
+
+        myFragmentManager = getSupportFragmentManager();
+        diaryFragment = new DiaryFragment();
+        profileFragment = new ProfileFragment();
+        calendarFragment = new CalendarFragment();
+        if (savedInstanceState == null) {
+
+            FragmentTransaction fragmentTransaction = myFragmentManager
+                    .beginTransaction();
+
+            fragmentTransaction.add(R.id.container, diaryFragment);
+            fragmentTransaction.commit();
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        fragmentTransaction = myFragmentManager
+                .beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.action_diary: {
+                bundle = new Bundle();
+                diaryFragment.setArguments(bundle);
+
+                fragmentTransaction.replace(R.id.container, diaryFragment);
+                break;
+            }
+            case R.id.action_profile:{
+                bundle = new Bundle();
+                profileFragment.setArguments(bundle);
+
+                fragmentTransaction.replace(R.id.container, profileFragment);
+                break;
+            }
+            case R.id.action_calendar:{
+                bundle = new Bundle();
+                calendarFragment.setArguments(bundle);
+
+                fragmentTransaction.replace(R.id.container, calendarFragment);
+                break;
+            }
+        }
+        fragmentTransaction.commit();
+        return false;
+    }
+
+    //TODO:
+//    https://startandroid.ru/ru/courses/architecture-components/27-course/architecture-components/560-urok-27-navigation-navigationui.html
+//    https://habr.com/ru/post/416025/
+    public void onNavigationItemSelected(int i) {
+        if (i == 1) {
+            Intent intent = new Intent(this, StatisticActivity.class);
+            startActivity(intent);
+        }
+        if (i == 2) {
+            Intent intent = new Intent(this, DiaryAddItem.class);
+            startActivity(intent);
+        }
+    }
+}
---
+++ mDMP/clientwithbottmmenu/src/main/java/com/example/clientwithbottmmenu/ui/statistic/StatisticFragment.java
@@ -0,0 +1,49 @@
+package com.example.clientwithbottmmenu.ui.statistic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.clientwithbottmmenu.R;
+
+
+public class StatisticFragment extends Fragment {
+
+    private StatisticViewModel mViewModel;
+
+    public static StatisticFragment newInstance() {
+        return new StatisticFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        mViewModel = ViewModelProviders.of(this).get(StatisticViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_statistic, container, false);
+        final TextView textView = root.findViewById(R.id.text_stat);
+        mViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(StatisticViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
